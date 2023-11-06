931095152095e3054f896ba6d7d2c90f887978f3
Heizenberg <popov.roman91@gmail.com>
2023-11-06 11:15:14 +0200

    added simple error handling

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,28 @@ fn to_ctrl_byte(c: char) -> u8 {
     byte & 0b0001_1111
 }
 
+fn die(e: std::io::Error) {
+    panic!("{}", e)
+}
+
 fn main() {
     let _stdout = stdout().into_raw_mode().unwrap();
 
     for b in io::stdin().bytes() {
-        let b = b.unwrap();
-        let c = b as char;
-        if c.is_control() {
-            println!("{:?} \r", b);
-        } else {
-            println!("{:?} ({})\r", b, c);
+        match b {
+            Ok(b) => {
+                let c = b as char;
+                if c.is_control() {
+                    println!("{:?} \r", b);
+                } else {
+                    println!("{:?} ({})\r", b, c);
+                }
+                if b == to_ctrl_byte('q') {
+                    break;
+                }
+            }
+            Err(err) => die(err)
         }
-        if b == to_ctrl_byte('q') {
-            break;
-        }
+        
     }
 }
