4566d29feb96bd55168a41377bcee1ce6102d23a
Popov Roman <popov.roman91@gmail.com>
2023-11-28 18:25:04 +0200

    added possibility to edit the file, without saving for now

--- src/row.rs
+++ src/row.rs
@@ -1,6 +1,8 @@
 use std::cmp;
 use unicode_segmentation::UnicodeSegmentation;
 
+
+#[derive(Default)]
 pub struct Row {
     string: String,
     len: usize,
@@ -47,4 +49,18 @@ impl Row {
     fn update_len(&mut self) {
         self.len = self.string[..].graphemes(true).count();
     }
+
+    pub fn insert(&mut self, at: usize, c: char) {
+        if at >= self.len() {
+            self.string.push(c);
+        } else {
+            let mut result: String = self.string[..].graphemes(true).take(at).collect();
+            let reminder: String = self.string[..].graphemes(true).skip(at).collect();
+            result.push(c);
+            result.push_str(&reminder);
+            self.string = result;
+        }
+        self.update_len();
+    }
+
 }
